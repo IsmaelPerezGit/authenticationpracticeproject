cb9344e3bc5f1ca261a5a55efd8c8e58bb79f3c9
Ismael Perez <ismaelperezemail@gmail.com>
2018-06-11 20:57:14 -0700

    connected mongodb to project

--- server/models/user.js
+++ server/models/user.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 
 // Define our model
 const userSchema = new Schema({
-   email: { type: String, unique: true, lowercase: true},
-   password: String
+    email: {type: String, unique: true, lowercase: true},
+    password: String
 });
 
 // Create a model class
