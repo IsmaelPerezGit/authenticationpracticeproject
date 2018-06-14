ee5cad2d36c435c5d217ba4060409b6aa2d1123f
Ismael Perez <ismaelperezemail@gmail.com>
2018-06-14 13:33:21 -0700

    added JWT encoding

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,4 +1,11 @@
 const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+function tokenForUser(user) {
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret)
+}
 
 exports.signup = function (req, res, next) {
     const email = req.body.email;
@@ -32,6 +39,6 @@ exports.signup = function (req, res, next) {
         });
 
         // Respond to request indicating the user was created
-        res.json(user)
+        res.json({ token: tokenForUser(user)})
     })
 };
